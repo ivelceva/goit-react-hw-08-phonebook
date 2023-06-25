85e6f9bf8889dbd87d92086ca7d76d7084f4cf14
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-25 11:13:18 -0700

    register

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { fetchContacts } from 'redux/contacts/operations';
+import { fetchContacts } from 'redux/ContactsOperations';
 import { ContactForm } from '../components/contactForm/ContactForm';
 import { ContactList } from '../components/contactList/ContactList';
 
---
+++ src/pages/Register.js
@@ -0,0 +1,5 @@
+import { RegisterForm } from 'components/signUpForm/SignUp';
+
+const Login = () => <RegisterForm />;
+
+export default Login;
