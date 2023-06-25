b50f7db1581fe4831921270a5a1f5f91c4444f6f
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-25 09:24:19 -0700

    style

--- src/components/layouts/Layouts.js
+++ src/components/layouts/Layouts.js
@@ -2,12 +2,12 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { UserMenu } from 'components/userMenu/UserMenu';
 import { Loader } from '../loader/Loader';
-//import css from './Layouts.module.css';
+import css from './Layouts.module.css';
 
 const Layouts = () => {
   return (
-    <div>
-      {/* <h1 className={css.title}>Phonebook</h1> */}
+    <div className={css.menuContainer}>
+      <h1 className={css.title}>Phonebook</h1>
       <UserMenu />
       <Suspense fallback={<Loader />}>
         <Outlet />
