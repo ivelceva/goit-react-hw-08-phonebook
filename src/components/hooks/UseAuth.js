ecbb1e1a4cbb161ee20904d4590c94f6e1b0cb9c
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-26 15:05:30 -0700

    auth

---
+++ src/components/hooks/UseAuth.js
@@ -0,0 +1,14 @@
+import { useSelector } from 'react-redux';
+import { selectUser, selectIsLoggedIn, selectIsFetchingCurrentUser } from '../redux/auth/selectors';
+
+export const useAuth = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isFetchingCurrentUser = useSelector(selectIsFetchingCurrentUser);
+  const user = useSelector(selectUser);
+
+  return {
+    isLoggedIn,
+    isFetchingCurrentUser,
+    user,
+  };
+};
